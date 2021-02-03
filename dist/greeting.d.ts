1f36eecd2924b88fa04179237fc9940a669ada59
lalosh <louayalosh17@gmail.com>
2021-02-03 14:16:27 +0200

    add js sdk of lableb

--- dist/greeting.d.ts
+++ dist/greeting.d.ts
@@ -3,5 +3,7 @@ import './main.css';
 export declare function GreetPerson({ name }: {
     name?: string;
 }): JSX.Element;
-export declare function SearchPerson(): JSX.Element;
+export declare function SearchPerson({ options }: {
+    options?: any;
+}): JSX.Element;
 export declare function TestImage(): JSX.Element;
--- src/greeting.tsx
+++ src/greeting.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import './main.css';
+import * as LablebSDK from '@lableb/javascript-sdk';
 const testImage = require('./test.png')
+
 
 
 export function GreetPerson({ name }: { name?: string }) {
     return <h1>{`Hello Again ${name || 'stranger'}!`}</h1>;
 }
 
-export function SearchPerson() {
+export function SearchPerson({ options }: { options?: any }) {
     const [search, setSearch] = useState('');
+
+    let client = LablebSDK.init(options);
 
     return <>
         <input
@@ -18,7 +22,16 @@ export function SearchPerson() {
             placeholder={'Search...'}
         />
         <button
-            onClick={() => console.log({ search })}
+            onClick={() => {
+                try {
+                    console.log({ search })
+                    client.search({
+                        query: search
+                    });
+                } catch (error) {
+                    console.error(error)
+                }
+            }}
         >{'search'}</button>
     </>
 }
